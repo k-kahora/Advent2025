b2a3e2784911023ac5564c3df19a7bfdf7279f8b
k-kahora <mango22mjk@gmail.com>
2025-12-05 15:11:50 -0500

    works but weird

--- lib/day3.ml
+++ lib/day3.ml
@@ -26,16 +26,16 @@ module Part1 = struct
   (* NOTE pre compute maximum suffix at each character non inclusive start from the back *)
   let joltage_suffix_list : int list -> int list =
    fun bank ->
-    List.rev bank |> fun [@ocaml.warning "-8"] (a :: b :: rest) ->
+    List.rev bank |> fun [@ocaml.warning "-8"] (a :: _b :: rest) ->
     List.fold_left
       (fun maximum_jolt_list batterie ->
         let max_jolt = List.hd maximum_jolt_list in
         if batterie > max_jolt then batterie :: maximum_jolt_list
         else max_jolt :: maximum_jolt_list)
-      [ a; b; 0 ] rest
+      [ a; a; a ] rest
 
   let maximum_joltage : int list -> int list -> int =
-   fun joltage_maximum_list bank ->
+   fun bank joltage_maximum_list ->
     Base.List.fold2_exn bank joltage_maximum_list ~init:0
       ~f:(fun max_jolt batterie best_jolt ->
         max max_jolt @@ ((batterie * 10) + best_jolt))
@@ -43,19 +43,29 @@ module Part1 = struct
   let total_joltage : int list -> int =
    fun joltages -> List.fold_left ( + ) 0 joltages
 
-  let rec remove_last = function
+  let rec replace_last_0 = function
     | [] -> []
-    | [ _a ] -> []
-    | hd :: rest -> hd :: remove_last rest
+    | [ _a ] -> [ 0 ]
+    | hd :: rest -> hd :: replace_last_0 rest
 
-  let part1 () =
-    let banks = get_banks test_input |> banks_to_int_list in
+  let remove_last_and_first lst =
+    let rec remove_last = function
+      | [] -> []
+      | [ _a ] -> []
+      | hd :: rest -> hd :: remove_last rest
+    in
+    let[@ocaml.warning "-8"] (_ :: rest) = lst in
+    remove_last rest
+
+  let part1 input =
+    let banks = get_banks input |> banks_to_int_list in
     let s =
       List.map
         (fun a ->
-          let suffix_list = joltage_suffix_list a |> remove_last in
-          let bank = 0 :: a |> remove_last in
-          let result = maximum_joltage suffix_list bank in
+          let[@ocaml.warning "-8"] (_ :: suffix_list) = joltage_suffix_list a in
+          (* let bank = 0 :: a |> remove_last in *)
+          let bank = a |> replace_last_0 in
+          let result = maximum_joltage bank suffix_list in
           Printf.printf "result -> %d\n" result;
           print_lists bank suffix_list;
           result)
@@ -69,15 +79,18 @@ open Base
 open Stdio
 
 let%expect_test "[day3-part1]" =
-  print_s [%sexp (part1 () : int)];
+  print_s [%sexp (part1 test_input : int)];
   [%expect {| (9 9 9 9 9 9 9 2 2 2 2 2 1 1 1) |}]
 
 let%expect_test "[day3] maximum joltage" =
-  let bank = bank_to_int_list "818181911112111" in
-  let suffix_list = bank |> joltage_suffix_list in
-  let output = 0 :: bank |> maximum_joltage suffix_list in
-  print_s [%sexp (0 :: bank : int list)];
+  let bank = bank_to_int_list "818911211" in
+  let[@ocaml.warning "-8"] (_ :: suffix_list) = bank |> joltage_suffix_list in
+  let[@ocaml.warning "-8"] bank = bank in
+  print_s [%sexp (bank : int list)];
   print_s [%sexp (suffix_list : int list)];
+  let output = maximum_joltage bank suffix_list in
+  (* print_s [%sexp (bank : int list)]; *)
+  (* print_s [%sexp (suffix_list : int list)]; *)
   print_s [%sexp (output : int)];
   [%expect {| (9 9 9 9 9 9 9 2 2 2 2 2 1 1 1) |}]
 
