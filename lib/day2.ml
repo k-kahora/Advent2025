10390309fd7330e9ec3e2671362950aae3c20702
k-kahora <mango22mjk@gmail.com>
2025-12-04 19:28:47 -0500

    day2 done

--- lib/day2.ml
+++ lib/day2.ml
@@ -32,7 +32,7 @@ let chunkify input =
   (* NOTE find the split of the string, then go through by chunks up to half of the string length, *)
   (* NOTE for each chunck compare it to the rest of the string if at any point its not equal early return*)
   (* NOTE  otherwise keep it and continue to the next chunk until there is none left then its invalid *)
-  let permutations, split_string =
+  let permutations, _split_string =
     let length = String.length input / 2 in
     let split_string = String.sub input 0 length in
     let permutations =
@@ -40,7 +40,28 @@ let chunkify input =
     in
     (permutations, split_string)
   in
-  (permutations, split_string)
+  (* Loop over eache permutaion and do chunk wis comparsions use fold_until  *)
+  (* Base.(Stdio.(print_s [%sexp (permutations : string list)])); *)
+  List.fold_left
+    (fun res permutation ->
+      if res then res
+      else
+        let perm_size = String.length permutation in
+        let string_length = String.length input in
+        if string_length mod perm_size <> 0 then false
+        else
+          let permutation_length = String.length permutation in
+          let chunk = string_length / permutation_length in
+          (* Printf.printf "input l -> %d, perm_length -> %d" string_length *)
+          (* permutation_length; *)
+          let result =
+            List.init chunk (fun i ->
+                permutation = String.sub input (i * perm_size) perm_size)
+            |> List.fold_left ( && ) true
+          in
+          (* Printf.printf "result -> %b\n" result; *)
+          result)
+    false permutations
 
 let ripped_apart_two input_list =
   String.split_on_char ',' input_list
@@ -54,6 +75,7 @@ let ripped_apart_two input_list =
          List.init (finish - start + 1) (fun i -> i + start))
   |> List.flatten
   |> List.map string_of_int (* |> List.filter chunkify *)
+  |> List.filter chunkify |> List.map int_of_string |> List.fold_left ( + ) 0
 
 open Base
 open Stdio
@@ -61,17 +83,18 @@ open Stdio
 let%expect_test "chunkify" =
   (* let real_input = Advent.Input.get_input ~year:2025 ~day:02 in *)
   (* let real_input = Input.get_input ~year:2025 ~day:02 in *)
-  let output = chunkify "446446" in
+  let output = chunkify "2121212118" in
   (* let output = ripped_apart real_input in *)
-  print_s [%sexp (output : string list * string)];
+  print_s [%sexp (output : bool)];
   [%expect {| 1 |}]
 
 let%expect_test "part2" =
   (* let real_input = Advent.Input.get_input ~year:2025 ~day:02 in *)
   (* let real_input = Input.get_input ~year:2025 ~day:02 in *)
-  let output = ripped_apart_two input_string in
+  let real_input = Input.get_input ~year:2025 ~day:02 in
+  let output = ripped_apart_two real_input in
   (* let output = ripped_apart real_input in *)
-  print_s [%sexp (output : string list)];
+  print_s [%sexp (output : int)];
   [%expect {| 4174379265 |}]
 
 let%expect_test "part1" =
